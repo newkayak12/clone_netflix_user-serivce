f5d0c99126e615d19a5a022e38662a0db7db8f42
kimSanghyun <sanghyun.kim@appg.co.kr>
2023-05-04 10:39:50 +0900

    init

---
+++ src/main/java/com/netflix_clone/userservice/WakeUp.java
@@ -0,0 +1,20 @@
+package com.netflix_clone.userservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created on 2023-05-04
+ * Project user-service
+ */
+@Component
+@Slf4j
+public class WakeUp {
+
+    @EventListener(value = {ApplicationReadyEvent.class})
+    public void message(){
+        log.warn("is ready");
+    }
+}
